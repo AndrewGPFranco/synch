ae20a91754f9a927bbc482ecfb5349755a1d02a4
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-07-06 02:38:03 -0300

    feat: add 2 novos casos de exception no handler do controller

--- src/main/java/com/drew/synch/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/drew/synch/controllers/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.drew.synch.controllers;
 
+import com.drew.synch.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,6 +14,18 @@ class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(RuntimeException.class)
     public Map<String, Object> handleException(RuntimeException ex) {
+        return Map.of("error", ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public Map<String, Object> handleException(NotFoundException ex) {
+        return Map.of("error", ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public Map<String, Object> handleException(Exception ex) {
         return Map.of("error", ex.getMessage());
     }
 
